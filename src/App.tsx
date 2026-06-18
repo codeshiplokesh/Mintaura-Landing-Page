766d6c21d921a353e93f04ada4687f1596bd2ff0
lokesh <lokesh.codeship@gmail.com>
2026-06-18 13:27:35 +0530

    Move FaqSection to be the last section in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,8 @@ function App() {
       <EcosystemSection />
       <ParticipantsSection />
       <DealsSection />
-      <FaqSection />
       <LeadSection />
+      <FaqSection />
     </div>
   );
 }
